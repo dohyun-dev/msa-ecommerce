fbc8d7486f8b6af84ec239cdef10669644b3d13a
권도현 <88278345+dohyun-dev@users.noreply.github.com>
2022-11-30 14:39:04 +0900

    feat: user-service (#2)
    * project init
    
    * feat: set user-service config
    
    * feat: create user
    
    * feat : feat health_check controller add port
    
    * feat : apigateway-service init
    
    * feat: finish develop user-service

---
+++ user-service/src/main/java/com/example/userservice/dto/UserDto.java
@@ -0,0 +1,19 @@
+package com.example.userservice.dto;
+
+import com.example.userservice.vo.OrderResponse;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class UserDto {
+    private String email;
+    private String name;
+    private String pwd;
+    private String userId;
+    private LocalDateTime createdAt;
+    private String encryptedPwd;
+
+    private List<OrderResponse> orders;
+}
